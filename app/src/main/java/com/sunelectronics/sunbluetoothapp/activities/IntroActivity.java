8f9b5a1587d219c94b7aee97dce33d0d89ae0b96
jg2004 <jgdeveloper2004@gmail.com>
2017-12-10 17:10:51 -0500

    added gradient backgrounds and IntroActivity layout

---
+++ app/src/main/java/com/sunelectronics/sunbluetoothapp/activities/IntroActivity.java
@@ -0,0 +1,15 @@
+package com.sunelectronics.sunbluetoothapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sunelectronics.sunbluetoothapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
